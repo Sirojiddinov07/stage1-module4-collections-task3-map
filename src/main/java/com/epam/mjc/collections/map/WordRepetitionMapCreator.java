d4f69a8997e7ed51d5f3a06acf372dda6064f699
Firstname Lastname <ssirojiddinov777@gmail.com>
2023-07-06 15:05:56 +0500

    first commit

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+        if (sentence.length() == 0)
+            return result;
+
+        String[] words =sentence.toLowerCase().replaceAll("[,.]", "").split(" ");
+        for (String word :words){
+            if (result.containsKey(word))
+                result.put(word, result.get(word) + 1);
+            else
+                result.put(word, 1);
+        }
+        return result;
     }
 }
